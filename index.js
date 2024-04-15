ff7afa81b4e7f66fad398d92e6fe032b974c0025
Moosadeveloper <janmoosa77@gmail.com>
2024-04-15 21:51:00 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var city = "Karachi";
+if (city === "Karachi") {
+    console.log("The City OF Lights");
+}
+
+if (x === y) {
+    var z = prompt("Enter the value of z:");
+}
+
+var ZipCode = prompt("10010");
+
+if (ZipCode === "10010") {
+    alert("karachi");
+} else {
+    alert("Please write correct city");
+}
+
+var x = 2;
+if (x === 2) { 
+    x = 1; 
+}
